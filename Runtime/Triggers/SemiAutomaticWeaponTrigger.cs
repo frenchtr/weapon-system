f049e73565542bdbf249ba13341681fec020a27d
Travis French <travis.french.1991@gmail.com>
2022-11-28 04:49:36 -0800

    Added semi-automatic weapon trigger.

---
+++ Runtime/Triggers/SemiAutomaticWeaponTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TravisRFrench.WeaponSystem.Runtime.Triggers
+{
+    public abstract class SemiAutomaticWeaponTrigger<TContext> : WeaponTrigger<TContext>
+    {
+        public override event Action<TContext> Triggered;
+
+        public override void Pull(TContext context = default(TContext))
+        {
+            base.Pull(context);
+            
+            this.Triggered?.Invoke(context);
+        }
+    }
+}
